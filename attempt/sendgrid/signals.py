b21a7655578c4ca13f3baca202ff9d14e479180d
Paul Olsen <profolsen@users.noreply.github.com>
2022-02-19 00:16:35 -0500

    Delete attempt/sendgrid directory
    this shouldn't be part of the repository right now.

--- attempt/sendgrid/signals.py
+++
@@ -1,3 +0,0 @@
-import django.dispatch
-
-email_event = django.dispatch.Signal()
--- attempt/sendgrid/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from .views import SendgridHook
-
-urlpatterns = [
-    url(r'^sendgrid_callback/$', SendgridHook.as_view()),
-]
