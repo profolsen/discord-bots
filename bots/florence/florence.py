ee47465c11a56be731a0a4a021f0acfafed82c96
Paul Olsen <olsenp@strose.edu>
2022-02-20 20:08:44 -0500

    bot is now working.  Time for a readme...

--- bots/florence/florence.py
+++ bots/florence/florence.py
@@ -2,12 +2,17 @@ import discord
 import sys
 from bots.tokens_util import load_token_from_file
 from datetime import datetime
+import sqlite3
+from bots import sqlutil
 token = load_token_from_file('florence')
 
 client = discord.Client()
 @client.event
 async def on_ready():
-    print('Logged in. Waiting...')
+    conn = sqlutil.connect(True)
+    sqlutil.reset(conn)
+    conn.close()
+    print('Database Ready...')
 
 #message events:
 #update category [attribute [...]]
@@ -21,28 +26,23 @@ async def on_message(message) :
     parts = message.content.split()
     end = None
     parts = parts[1:end] #ignore command.
+    conn = sqlutil.connect(False)
     if message.content.lower().startswith('update') :
-        with open(sys.argv[1], 'a+') as output:
-            for word in parts :
-                output.write(word + "\n")
-            output.write('updates\n')
-            output.write('*' + str(datetime.now().timestamp()) + '\n')
-    if message.content.lower().startswith('report') :
-        if parts[0] == 'now' :
-            try:
-                with open(sys.argv[1]) as input:
-                    lines = input.readlines()
-                    wordcount = {}
-                    for line in lines :
-                        if not line.startswith('*') :
-                            pureline = line[0:len(line) - 1]
-                            wordcount[pureline] = wordcount[pureline] + 1 if pureline in wordcount else 1
-                    await message.channel.send(str(wordcount))
-            except FileNotFoundError :
-                await message.channel.send('no updates found.')
-
-
+        sqlutil.update(conn, parts[0], parts[1:end])
         pass
+    if message.content.lower().startswith('report') :
+        reporttype = parts[0]
+        parts = parts[1:end]
+        response = 'Internal Error'
+        if reporttype == 'now' :
+            response = sqlutil.reportnow(conn)
+        elif reporttype == 'daily' :
+            response = sqlutil.dailydata(conn, parts[0], parts[1], parts[2:end])
+        elif reporttype == 'diff' :
+            response = sqlutil.timediff(conn, parts[0], parts[1])
+        await message.channel.send(response)
+    conn.close()
+        #message.channel.send(str(wordcount))
         #await message.channel.send(message.content[11:])
 
 if __name__ == "__main__" :
